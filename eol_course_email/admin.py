6865eaa9653217278a032f625541372530249cf7
Matías Salinas <matsalinas@uchile.cl>
2021-05-28 16:52:11 +0000

    Update django admin users view

--- eol_course_email/admin.py
+++ eol_course_email/admin.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 from .models import EolCourseEmail, FilesCourseEmail
 
 class EolCourseEmailSetupAdmin(admin.ModelAdmin):
+    raw_id_fields = ('sender_user', 'receiver_users')
     list_display = ('created_at', 'course_id', 'sender_user', 'subject')
 
 class FilesCourseEmailSetupAdmin(admin.ModelAdmin):
